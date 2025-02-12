045e86623d2da860d889a8d9f152f3176489998e
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-11 22:22:09 -0600

    initialize app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { Login } from './pages/login/login.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Login,
+    pathMatch: 'full'
+  }
+]
