f6e0348562f86c47eb42a5862db5c07d1e14115e
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-13 11:33:42 -0600

    add footer

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'chp-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class Footer {
+
+}
