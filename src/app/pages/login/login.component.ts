f10356af267a70d466516157dc61d5e166162e6d
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-13 11:55:36 -0600

    update login UI password reset link

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,9 +7,10 @@ import { PasswordModule } from 'primeng/password'
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'
 import { ProgressSpinnerModule } from 'primeng/progressspinner'
+import { Footer } from '../../components/footer/footer.component'
 
 @Component({
-  selector: 'chd-login',
+  selector: 'chp-login',
   standalone: true,
   imports: [
     InputTextModule,
@@ -18,7 +19,8 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner'
     CheckboxModule,
     ProgressSpinnerModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Footer
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -27,14 +29,28 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner'
 export class Login implements OnInit {
   public sharedService = inject(SharedService)
   public primengConfig = inject(PrimeNGConfig)
+  public forgotPassword = signal<boolean>(false)
+  public resetLinkSent = signal<boolean>(false)
   public errorMessage = signal<string>('')
   public loginForm = new FormGroup({
     email: new FormControl('', [Validators.email, Validators.required]),
     password: new FormControl('', Validators.required),
     checked: new FormControl('')
   })
+  public resetPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required])
+  })
 
   ngOnInit(): void {
     this.primengConfig.ripple = true
+  }
+
+  public sendResetLink(): void {
+    this.sharedService.loading.set(true)
+
+    setTimeout(() => {
+      this.resetLinkSent.set(true)
+      this.sharedService.loading.set(false)
+    }, 1500)
   }
 }
