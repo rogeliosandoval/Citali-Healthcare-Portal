3624d3a70a68c35953845a12e94e177bad896037
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-17 22:07:07 -0600

    update main styles and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router'
 import { Login } from './pages/login/login.component'
 import { Dashboard } from './pages/dashboard/dashboard.component'
+import { Database } from './pages/dashboard/database/database.component'
+import { Inbox } from './pages/dashboard/inbox/inbox.component'
 
 export const routes: Routes = [
   {
@@ -11,5 +13,10 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: Dashboard,
+    children: [
+      { path: 'database', component: Database },
+      { path: 'inbox', component: Inbox },
+      { path: '**', redirectTo: 'database', pathMatch: 'full' }
+    ]
   }
 ]
