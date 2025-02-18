91804f64a93e41bcc602c565fc2e141c88041884
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-17 23:33:45 -0600

    clean up config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
