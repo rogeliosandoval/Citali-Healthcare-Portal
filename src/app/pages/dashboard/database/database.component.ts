c05ec9df088a96800fa2ea45565f4a24f19071e3
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-17 22:06:34 -0600

    add database page

---
+++ src/app/pages/dashboard/database/database.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'chp-database',
+  standalone: true,
+  imports: [],
+  templateUrl: './database.component.html',
+  styleUrl: './database.component.scss'
+})
+
+export class Database implements OnInit {
+  ngOnInit(): void {
+      
+  }
+}
