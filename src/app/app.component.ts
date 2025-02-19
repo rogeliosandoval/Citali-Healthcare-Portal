a69d0a90f8061164d341bc1fd420221c46c2f5bc
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-18 18:10:27 -0600

    update main app component to work with auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { SharedService } from './services/shared.service'
+import { CommonModule } from '@angular/common'
+import { AuthService } from './services/auth.service'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'citali-healthcare-portal';
+export class AppComponent implements OnInit {
+  public authService = inject(AuthService)
+  public sharedService = inject(SharedService)
+  public loadApp = signal<boolean>(false)
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user: any) => {
+      if (user) {
+        this.authService.currentUserSignal.set({
+          email: user.email!,
+          name: user.displayName!,
+        })
+        this.loadApp.set(true)
+      } else {
+        this.authService.currentUserSignal.set(null)
+        this.loadApp.set(true)
+      }
+    })
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router'
 import { Login } from './pages/login/login.component'
+import { Signup } from './pages/signup/signup.component'
 import { Dashboard } from './pages/dashboard/dashboard.component'
+import { AuthUserGuard } from './guards/auth.user.guard'
+import { AuthNonuserGuard } from './guards/auth.nonuser.guard'
 import { Database } from './pages/dashboard/database/database.component'
 import { Inbox } from './pages/dashboard/inbox/inbox.component'
 
@@ -8,15 +11,26 @@ export const routes: Routes = [
   {
     path: '',
     component: Login,
+    canActivate: [AuthNonuserGuard],
     pathMatch: 'full'
   },
   {
     path: 'dashboard',
     component: Dashboard,
+    canActivate: [AuthUserGuard],
     children: [
       { path: 'database', component: Database },
       { path: 'inbox', component: Inbox },
       { path: '**', redirectTo: 'database', pathMatch: 'full' }
     ]
+  },
+  {
+    path: 'register',
+    component: Signup,
+    canActivate: [AuthNonuserGuard]
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ]
