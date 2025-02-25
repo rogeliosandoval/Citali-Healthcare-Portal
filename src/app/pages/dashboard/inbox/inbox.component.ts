ab641545e16b802d15845ece04f5f6ff85d19d6d
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2025-02-24 18:40:49 -0600

    clean inbox

--- src/app/pages/dashboard/inbox/inbox.component.ts
+++ src/app/pages/dashboard/inbox/inbox.component.ts
@@ -1,11 +1,10 @@
-import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'chp-inbox',
   standalone: true,
   imports: [
-    RouterLink
+
   ],
   templateUrl: './inbox.component.html',
   styleUrl: './inbox.component.scss'
